08be4d24518f65a618eef7bd5a924c71e76cb5f8
Felix Kiss <felix@felixkiss.com>
2018-10-01 19:16:16 +0200

    Question 2 (2.1 + 2.2)

--- README.md
+++ README.md
@@ -24,3 +24,28 @@ LIMIT 1
 ```
 
 (see commit for all details)
+
+## 2. We define a ​Blog​ RoR application in which users can write Posts and they can Comment on Posts.
+
+### 2.1. How would you build the schema for the database tables that we need for the Blog application described above?
+
+```
+rails generate model User
+rails generate model Post user:references:index content:text
+rails generate model Comment user:references:index post:references:index content:text
+rails db:migrate
+```
+
+### 2.2. How do you define the Rails models corresponding to the tables above? What associations are declared and how?
+
+The belongs_to associations are already defined by the generators above. We need to manually specify the inverse of the belongs_to associations.
+
+Full list of associations:
+ - Post belongs_to :user
+ - User has_many :posts (needs to added manually)
+ - Comment belongs_to :post
+ - Comment belongs_to :user
+ - User has_many :comments (needs to added manually)
+ - Post has_many :comments (needs to added manually)
+
+See commit for all model classes.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_154225) do
+ActiveRecord::Schema.define(version: 2018_10_01_170414) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "payments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
@@ -20,10 +30,21 @@ ActiveRecord::Schema.define(version: 2018_10_01_154225) do
     t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "payments", "users"
+  add_foreign_key "posts", "users"
 end
